46d4220204e82ebee0f3cb52a94327c0f19993cf
MiKaa339 <154283412+MiKaa339@users.noreply.github.com>
2023-12-18 20:02:58 -0300

    Delete Calcula Boletim.py

--- Calcula Boletim.py
+++
@@ -1,7 +0,0 @@
-nome = input ('Olá, qual o seu nome?')
-print ('Seja bem-vindo (a) {}, vamos calcular sua média bimestral do boletim?'.format(nome))
-n1 = float ( input ( 'Primeiro, me diga sua nota da prova mensal:'))
-n2 = float ( input ( 'Agora, me diga sua nota da prova bimestral:'))
-n3 = float ( input ( 'Por fim, digite sua nota de atividade do bimestre:'))
-média = (((n1 * 2) + (n2 * 2)) + n3) / 5
-print ('Calculei aqui, e sua nota bimestral será de: {}'.format(média))
